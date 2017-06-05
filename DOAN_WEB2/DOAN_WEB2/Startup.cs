c46b51beb983a160eaf284fa5df2dc472d7b1407
Phat Dang <danghuuphat1601@gmail.com>
2017-06-05 10:24:57 +0700

    Layout Nguoi dung

---
+++ DOAN_WEB2/DOAN_WEB2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DOAN_WEB2.Startup))]
+namespace DOAN_WEB2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
